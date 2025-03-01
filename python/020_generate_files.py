90452555be6e2677197da46c14bcc983c41dcdb9
wegamekinglc <scrappedprince.li@gmail.com>
2025-03-01 22:08:15 +0800

    update data file

--- python/020_generate_files.py
+++ python/020_generate_files.py
@@ -3,11 +3,11 @@ import pyarrow.parquet as pq
 import pyarrow.csv as pcsv
 
 tbl = pq.read_table("data/yellow_tripdata_2015-01.parquet")
-# pa.csv.write_csv(tbl, "data/yellow_tripdata_2015-01.csv")
+pa.csv.write_csv(tbl, "data/yellow_tripdata_2015-01.csv")
 
-# with pa.OSFile("data/yellow_tripdata_2015-01.arrow", "wb") as sink:
-#     with pa.RecordBatchFileWriter(sink, tbl.schema) as writer:
-#         writer.write_table(tbl)
+with pa.OSFile("data/yellow_tripdata_2015-01.arrow", "wb") as sink:
+    with pa.RecordBatchFileWriter(sink, tbl.schema) as writer:
+        writer.write_table(tbl)
 
 df = tbl.to_pandas().fillna(0)
 tbl = pa.Table.from_pandas(df)
