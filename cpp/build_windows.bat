3a693a241a112771a8c195ac4acf8dade00b1dc2
wegamekinglc <scrappedprince.li@gmail.com>
2025-03-01 23:15:32 +0800

    added windows bat

---
+++ cpp/build_windows.bat
@@ -0,0 +1,84 @@
+@echo off
+
+call :set_variable BUILD_TYPE Release %BUILD_TYPE%
+call :set_variable WORK_DIR "%CD%" %WORK_DIR%
+call :set_variable ADDRESS_MODEL Win64 %ADDRESS_MODEL%
+call :set_variable MSVC_RUNTIME static %MSVC_RUNTIME%
+call :set_variable MSVC_VERSION "Visual Studio 17 2022" %MSVC_VERSION%
+call :set_variable CMAKE_TOOLCHAIN_FILE "%CD%/vcpkg/scripts/buildsystems/vcpkg.cmake" %CMAKE_TOOLCHAIN_FILE%
+
+echo BUILD_TYPE:  %BUILD_TYPE%
+echo WORK_DIR: %WORK_DIR%
+echo ADDRESS_MODEL: %ADDRESS_MODEL%
+echo MSVC_RUNTIME: %MSVC_RUNTIME%
+echo MSVC_VERSION: %MSVC_VERSION%
+echo CMAKE_TOOLCHAIN_FILE: %CMAKE_TOOLCHAIN_FILE%
+
+git submodule init
+git submodule update
+
+cd vcpkg
+echo Starting vcpkg install gtest
+if exist "./vcpkg.exe" (
+    rem "vcpkg executable already exists"
+) else (
+    .\bootstrap-vcpkg.bat
+)
+
+.\vcpkg install arrow:x64-windows-static
+
+if %errorlevel% neq 0 exit /b 1
+cd ..
+
+if "%ADDRESS_MODEL%"=="Win64" (
+  set PLATFORM=x64
+) else (
+  if "%MSVC_VERSION%"=="Visual Studio 16 2019" (
+    set PLATFORM=x64
+  ) else (
+    if "%MSVC_VERSION%"=="Visual Studio 17 2022" (
+        set PLATFORM=x64
+    ) else (
+        set PLATFORM=Win32
+    )
+  )
+)
+
+if "%ADDRESS_MODEL%"=="Win64" (
+  if "%MSVC_VERSION%"=="Visual Studio 16 2019" (
+    set ADDRESS_MODEL=
+  ) else (
+    if "%MSVC_VERSION%"=="Visual Studio 17 2022" (
+        set ADDRESS_MODEL=
+    )
+  )
+)
+
+if exist build (
+  rem build folder already exists.
+) else (
+  mkdir build
+)
+
+cd build
+if "%ADDRESS_MODEL%"=="Win64" (
+cmake -G "%MSVC_VERSION% %ADDRESS_MODEL%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%WORK_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE% -DVCPKG_TARGET_TRIPLET=x64-windows-static ..
+) else (
+cmake -G "%MSVC_VERSION%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%WORK_DIR% -DMSVC_RUNTIME=%MSVC_RUNTIME% -DCMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE% -DVCPKG_TARGET_TRIPLET=x64-windows-static ..
+)
+
+if %errorlevel% neq 0 exit /b 1
+
+msbuild Arrow-Practices.sln /m /p:Configuration=%BUILD_TYPE% /p:Platform=%PLATFORM%
+msbuild INSTALL.vcxproj /m:%NUMBER_OF_PROCESSORS% /p:Configuration=%BUILD_TYPE% /p:Platform=%PLATFORM%
+
+if %errorlevel% neq 0 exit /b 1
+
+cd ..
+
+@echo on
+
+EXIT /B 0
+
+:set_variable
+set %~1=%~2
