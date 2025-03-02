8872d9295d41b9193c4756fba64dedab6a31d1b6
wegamekinglc <scrappedprince.li@gmail.com>
2025-03-03 00:25:54 +0800

    fixed

--- cpp/chapter7/flight_client.cpp
+++ cpp/chapter7/flight_client.cpp
@@ -7,17 +7,18 @@
 #include <arrow/flight/api.h>
 
 int main(int argc, char** argv) {
-    arrow::flight::Location location;
-    ARROW_ASSIGN_OR_RAISE(location,
-                          arrow::flight::Location::ForGrpcTcp("localhost", 62001));
-    std::unique_ptr<arrow::flight::FlightClient> client;
-    ARROW_ASSIGN_OR_RAISE(client, arrow::flight::FlightClient::Connect(location));
+    arrow::flight::Location location = arrow::flight::Location::ForGrpcTcp("localhost", 62001).ValueOrDie();
+    std::unique_ptr<arrow::flight::FlightClient> client = arrow::flight::FlightClient::Connect(location).ValueOrDie();
     std::cout << "Connected to " << location.ToString() << std::endl;
 
     auto descriptor = arrow::flight::FlightDescriptor::Path({"C:/Users/wegam/github/arrow-practices/python/data/yellow_tripdata_2015-01.parquet"});
-
-    std::unique_ptr<arrow::flight::FlightInfo> flight_info;
-    ARROW_ASSIGN_OR_RAISE(flight_info, client->GetFlightInfo(descriptor));
-    std::unique_ptr<arrow::flight::FlightStreamReader> stream;
-    ARROW_ASSIGN_OR_RAISE(stream, client->DoGet(flight_info->endpoints()[0].ticket));
+    std::unique_ptr<arrow::flight::FlightListing> flights_list = client->ListFlights().ValueOrDie();
+    while (true) {
+        std::unique_ptr<arrow::flight::FlightInfo> flight_info = flights_list->Next().ValueOrDie();
+        if (!flight_info) break;
+        std::unique_ptr<arrow::flight::FlightStreamReader> stream = client->DoGet(flight_info->endpoints()[0].ticket).ValueOrDie();
+        std::shared_ptr<arrow::Table> table = stream->ToTable().ValueOrDie();
+        arrow::PrettyPrintOptions print_options(/*indent=*/0, /*window=*/2);
+        auto res = arrow::PrettyPrint(*table, print_options, &std::cout);
+    }
 }
