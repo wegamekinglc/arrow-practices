59d46bde9fa2b298443a70a79afed450ecd5be89
wegamekinglc <scrappedprince.li@gmail.com>
2025-03-15 23:03:27 +0800

    update cpp example

--- cpp/chapter7/flight_client.cpp
+++ cpp/chapter7/flight_client.cpp
@@ -2,23 +2,32 @@
 // Created by wegam on 2025/3/2.
 //
 
-#include <iostream>
 #include <arrow/api.h>
 #include <arrow/flight/api.h>
+#include <iostream>
 
-int main(int argc, char** argv) {
-    arrow::flight::Location location = arrow::flight::Location::ForGrpcTcp("localhost", 62001).ValueOrDie();
-    std::unique_ptr<arrow::flight::FlightClient> client = arrow::flight::FlightClient::Connect(location).ValueOrDie();
+arrow::Status read_table_from_server(const std::string& work_dir)
+{
+    ARROW_ASSIGN_OR_RAISE(const auto location, arrow::flight::Location::ForGrpcTcp("localhost", 62001));
+    ARROW_ASSIGN_OR_RAISE(const auto client, arrow::flight::FlightClient::Connect(location));
     std::cout << "Connected to " << location.ToString() << std::endl;
 
-    auto descriptor = arrow::flight::FlightDescriptor::Path({"C:/Users/wegam/github/arrow-practices/python/data/yellow_tripdata_2015-01.parquet"});
-    std::unique_ptr<arrow::flight::FlightListing> flights_list = client->ListFlights().ValueOrDie();
+    auto descriptor = arrow::flight::FlightDescriptor::Path({work_dir + "/python/data/yellow_tripdata_2015-01.parquet"});
+    ARROW_ASSIGN_OR_RAISE(auto flights_list, client->ListFlights());
     while (true) {
-        std::unique_ptr<arrow::flight::FlightInfo> flight_info = flights_list->Next().ValueOrDie();
+        ARROW_ASSIGN_OR_RAISE(auto flight_info, flights_list->Next());
         if (!flight_info) break;
-        std::unique_ptr<arrow::flight::FlightStreamReader> stream = client->DoGet(flight_info->endpoints()[0].ticket).ValueOrDie();
-        std::shared_ptr<arrow::Table> table = stream->ToTable().ValueOrDie();
+        ARROW_ASSIGN_OR_RAISE(auto stream, client->DoGet(flight_info->endpoints()[0].ticket));
+        ARROW_ASSIGN_OR_RAISE(auto table, stream->ToTable());
         arrow::PrettyPrintOptions print_options(/*indent=*/0, /*window=*/2);
         auto res = arrow::PrettyPrint(*table, print_options, &std::cout);
     }
+    return arrow::Status::OK();
+}
+
+int main(int argc, char** argv) {
+    if (argc <= 1)
+        throw std::runtime_error("Usage: main arguments is missing");
+    const std::string work_dir = argv[1];
+    return static_cast<int>(read_table_from_server(work_dir).code());
 }
